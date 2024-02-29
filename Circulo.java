f2eeff3edf474cc93b198d564240cacfecaefa71
Mariluz Caro <159815558+mcaror17@users.noreply.github.com>
2024-02-29 09:54:51 -0500

    Circulo.java

--- Circulo.java
+++ Circulo.java
@@ -5,7 +5,7 @@
 import java.util.Scanner;
 public class Circulo {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         double r, area, per;
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingrese un numero: ");
