315232f4cd66de88bab51fe8d84ef0b8aae3a344
Mariluz Caro <159815558+mcaror17@users.noreply.github.com>
2024-02-29 10:09:02 -0500

    PruebaDeEscritorio.java

--- PruebaDeEscritorio.java
+++ PruebaDeEscritorio.java
@@ -4,7 +4,7 @@
  */
 public class PruebaDeEscritorio {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         double suma, x, y;
         suma = 0;
         x = 20;
