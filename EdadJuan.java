78ce198aac7cb664049a5eac2619b3eb28bc107e
Mariluz Caro <159815558+mcaror17@users.noreply.github.com>
2024-02-29 08:32:27 -0500

    Add files via upload

---
+++ EdadJuan.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author MARILUZ CARO
+ */
+public class EdadJuan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int edjuan = 9; 
+        int edana, edalber, edmama;
+        edalber = 2*edjuan/3;
+        edana = 4*edjuan/3;
+        edmama = edjuan + edalber + edana;
+        System.out.println("Las edades son: ");
+        System.out.println("Alberto = " + edalber);
+        System.out.println("Juan = " + edjuan);
+        System.out.println("Ana = " + edana);
+        System.out.println("Mama = " + edmama);
+
+        
+}
+}
+    
